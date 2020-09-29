c1c0357760f86ffc16f3ada7a73fa418f50098e3
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-29 10:26:32 -0300

    feat: modify TransactionList

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
+  final List<Transaction> _transactions = [
     Transaction(
       id: 't1',
       title: 'Tênis de corrida',
